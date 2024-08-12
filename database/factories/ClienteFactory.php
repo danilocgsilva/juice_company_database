acf9a99991c554919d9a290e694bc4b7902f68cb
Danilo Silva <contact@danilocgsilva.me>
2024-08-12 09:07:43 +0000

    wip factory cliente
    Mais valores aleatorios para o factory de cliente

--- database/factories/ClienteFactory.php
+++ database/factories/ClienteFactory.php
@@ -5,12 +5,42 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Faker\Factory as FFactory;
 use DateTime;
+use Illuminate\Support\Collection;
+use MathPHP\Probability\Distribution\Continuous\Normal;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Cliente>
  */
 class ClienteFactory extends Factory
 {
+    private Normal $normalDistribuitionMimiteCredito;
+    
+    public function __construct(
+        $count = null,
+        ?Collection $states = null,
+        ?Collection $has = null,
+        ?Collection $for = null,
+        ?Collection $afterMaking = null,
+        ?Collection $afterCreating = null,
+        $connection = null,
+        ?Collection $recycle = null
+    ) {
+        parent::__construct(
+            $count,
+            $states,
+            $has,
+            $for,
+            $afterMaking,
+            $afterCreating,
+            $connection,
+            $recycle
+        );
+
+        $mean = 100_000;
+        $stdDeviation = 10_000;
+        $this->normalDistribuitionMimiteCredito = new Normal($mean, $stdDeviation);
+    }
+    
     public function withFaker()
     {
         return FFactory::create('pt_BR');
@@ -23,25 +53,21 @@ class ClienteFactory extends Factory
      */
     public function definition(): array
     {
-        $cpfGerado = str_replace(".", "",
-            str_replace("-", "", $this->faker->cpf)
-        );
-        
         return [
             'CPF' => str_replace(".", "",
                 str_replace("-", "", $this->faker->cpf)
             ),
-            'NOME' => $this->faker->name,
+            'NOME' => $this->faker->name(($gender = rand(0, 1) === 0 ? 'F' : 'M')),
             'ENDERECO_1' => $this->faker->streetName,
             'ENDERECO_2' => $this->faker->streetName,
             'BAIRRO' => "Liberdade",
             'CIDADE' => $this->faker->city,
             'ESTADO' => "SP",
             'CEP' => str_replace("-", "", $this->faker->postcode),
-            'DATA_DE_NASCIMENTO'=> $this->generateRandomDate(),
-            'IDADE' => 22,
-            'SEXO' => 'M',
-            'LIMITE_DE_CREDITO' => 110000,
+            'DATA_DE_NASCIMENTO'=> ($dataNascimentoCliente = $this->generateRandomDate()),
+            'IDADE' => $this->calcularIdade($dataNascimentoCliente),
+            'SEXO' => $gender,
+            'LIMITE_DE_CREDITO' => round($this->normalDistribuitionMimiteCredito->rand()),
             'VOLUME_DE_COMPRA' => 22000,
             'PRIMEIRA_COMPRA' => 1
         ];
@@ -52,5 +78,11 @@ class ClienteFactory extends Factory
         $randomUnixTimestamp = rand(523421251, 1123421251);
 
         return DateTime::createFromFormat("U", $randomUnixTimestamp)->format('Y-m-d');
+    }
+
+    private function calcularIdade(string $dataString): int
+    {
+        $dataParts = explode("-", $dataString);
+        return (string) (new DateTime)->format('Y') - (int) $dataParts[0];
     }
 }
